55957b7e01893d780e5afd5c59184dcd4997390b
Matt Kiefer <matthewlkiefer@gmail.com>
2018-05-12 15:48:20 -0500

    docstring log module

--- log/log.py
+++ log/log.py
@@ -1,3 +1,13 @@
+"""
+handles logging for:
+    - auth
+    - contact
+    - msg
+    - label
+    - report
+    - att
+modules
+"""
 import csv
 from datetime import datetime
 import os, shutil
@@ -16,18 +26,31 @@ logpaths = {
 ### END CONFIG ###
 
 def log_data(logtype,data):
+    """
+    logs data to specified file based on logtype
+    """
     for datum in data:
         datum['timestamp'] = timestamp()
         datum = stringify_dict(datum)
     write_or_append(logtype,data)
         
 def timestamp():
+    """
+    stringifies current time
+    """
     return datetime.now().strftime('%Y-%m-%d_%T')
 
 def stringify_dict(datum):
+    """
+    returns log data with all values as strings
+    """
     return dict((x,str(datum[x])) for x in datum)
 
 def write_or_append(logtype,data):
+    """
+    checks if file exists and appends,
+    else creates and writes (starting with headers
+    """
     path = logpaths[logtype]
     method = 'w'
     if check_file_exists(logtype) and check_schema_match(logtype,data):
@@ -43,14 +66,25 @@ def write_or_append(logtype,data):
     logfile.close()
 
 def check_file_exists(logtype):
+    """
+    returns True if path exists
+    """
     return os.path.isfile(logpaths[logtype])
 
 def check_schema_match(logtype,data):
+    """
+    verifies existing file has same headers as data we're appending
+    """
     # check if new data matches logfile schema
     return sorted(data[0].keys()) == \
         sorted(csv.DictReader(logpaths[logtype]).fieldnames)
 
 def write_log(logfile,method,data):
+    """
+    writes data to specified file,
+    appending if it already exists
+    or writing if it doesn't
+    """
     logcsv = csv.DictWriter(logfile,data[0].keys())
     if method == 'w':
         logcsv.writeheader()
