9667418a677ebb8375fe3aedcdc3d8c100281b1a
Matt Kiefer <matthewlkiefer@gmail.com>
2018-05-12 13:59:03 -0500

    docstring auth module

--- auth/auth.py
+++ auth/auth.py
@@ -1,20 +1,27 @@
+"""
+authentication tools for:
+- contacts
+- gmail
+- sheets
+- drive
+that allow all other modules to work
+via wrapper functions
+"""
 import httplib2, sys, json
-
 from apiclient.discovery import build
 from oauth2client import tools
 from oauth2client.file import Storage
 from oauth2client.client import AccessTokenRefreshError
 from oauth2client.client import OAuth2WebServerFlow
-
 import atom.data
 import gdata.data
 import gdata.contacts.client
 import gdata.contacts.data
 
-
 ### START CONFIG ###
 client_id_path = 'auth/client_id_2017.json'
 credential_path = 'auth/credentials.dat'
+# some of these may be redundant
 scopes = (
             'https://www.googleapis.com/auth/gmail.labels',
             'https://www.google.com/m8/feeds',
@@ -26,7 +33,6 @@ scopes = (
 debug = True
 ### END CONFIG ###
 
-
 # setup
 storage = Storage(credential_path)
 client_json = json.load(open(client_id_path))['installed']
@@ -35,6 +41,12 @@ project_id = client_json['project_id']
 
 
 def get_cred():
+    """
+    triggers authorization workflow
+    to get credentials for scopes as configured
+    if they aren't already stored locally.
+    follow cli instructions for first-time setup
+    """
     credentials = storage.get()
     if credentials is None or credentials.invalid or \
             sorted([x for x in credentials.scopes]) != sorted([x for x in scopes]):
@@ -44,6 +56,12 @@ def get_cred():
 
 
 def get_service(credentials=get_cred(),type='gmail'):
+    """
+    uses credentials to get an http service object for
+    - gmail
+    - sheets
+    - drive
+    """
     http = credentials.authorize(httplib2.Http(disable_ssl_certificate_validation=True))
     if type == 'gmail':
         return build('gmail','v1',http=http)
@@ -53,6 +71,10 @@ def get_service(credentials=get_cred(),type='gmail'):
         return build('drive', 'v3', http=http)
 
 def get_gd_client(credentials=get_cred()):
+    """ 
+    uses credentials to get a gd_client service object for
+    - contacts
+    """
     gd_client = gdata.contacts.client.ContactsClient(source=project_id)
     gd_token = gdata.gauth.OAuth2TokenFromCredentials(credentials)
     gd_client.auth_token = gd_token
@@ -61,6 +83,14 @@ def get_gd_client(credentials=get_cred()):
 
 
 def test_cred(credentials=get_cred()):
+    """
+    a test function to verify auth/functionality of:
+    - contacts
+    - gmail
+    - drive
+    - sheets
+    some of these tests may ned to be reviewed
+    """
     # contacts
     gd_client = get_gd_client(credentials)
     if gd_client: print 'contact success'
