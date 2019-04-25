31b5a6634ad88a7c82d99eec5fc199000dddcb46
Patrick Judge <pjudge@bettergov.org>
2019-04-25 16:47:54 -0500

    predominantly aesthetic edits to msg.compose

--- msg/compose.py
+++ msg/compose.py
@@ -2,18 +2,16 @@
 creates and sends
 foia messages
 """
-from time import sleep
+import os
+from pathlib import Path
+from decouple import config
 from log import log
 from auth import auth
-from docx import Document
-from contacts.contacts import get_contacts_by_agency
-from msg.utils import agency_slug
-from msg.label import label_agency
-from email.mime.text import MIMEText
-import base64
 
 ### START CONFIG ###
-foia_doc = 'msg/payroll-foia-2018.docx'
+BASE_DIR = Path(__file__).parent.parent.absolute()
+foia_doc = os.path.join(BASE_DIR, config(
+    'MSG_TEMPLATE_PATH'))
 interval = 1  # seconds
 subject = 'Payroll FOIA | '
 me = 'me'
@@ -35,10 +33,27 @@ def distribute(send=False):
     *** retry logic is untested ***
     """
     retry = True
+
+    # TODO: prep new drafts? maybe we already have them made.
+    '''
+    prep_drafts = input('\nPrep new drafts ([y]/n)? ') or 'Y'
+    while not prep_drafts.lower() in ('y', 'n'):
+        print('Error! Answer only y or n.')
+        prep_drafts = input('\nPrep new drafts ([y]/n)? ') or 'Y'
+
+    prep_drafts = prep_drafts.lower() == 'y'
+
+    if prep_drafts:
+        drafts = prep_agency_drafts()
+    else:
+        drafts = get_drafts()
+    '''
+
     drafts = prep_agency_drafts()
+
     if send and sanity_check(drafts):
         ready = input(
-            'drafts created. inspect and type "send" to distribute')
+            'Inspect and type "send" to distribute: ')
         if ready.lower() == 'send':
             while retry:
                 original_draft_len = len(drafts)
@@ -69,7 +84,9 @@ def unsent_agency_contacts():
     contacts by agency
     for all unsent agencies
     """
+    from contacts.contacts import get_contacts_by_agency
     from report.response import get_threads
+
     contacts_by_agency = get_contacts_by_agency()
     return dict((agency, contacts_by_agency[agency]) for agency
                 in contacts_by_agency if not get_threads(agency))
@@ -89,17 +106,19 @@ def prep_agency_drafts(contacts_by_agency=[]):
     draft = [{'agency': agency_name,'draft': draft}]
 
     """
+    from msg.utils import agency_slug
+
     if not contacts_by_agency:
         contacts_by_agency = unsent_agency_contacts()
     # first delete existing drafts
     delete_drafts()
     # then create new drafts
     print('agencies to be prepped:', contacts_by_agency.keys())
-    pd = input('prep drafts now? [y/N]')
+    pd = input('prep drafts now (y/[n])? ') or 'n'
     if pd.lower() == 'y':
         foia_text = load_foia_text()
         drafts = []
-        for agency in contacts_by_agency:
+        for i, agency in enumerate(contacts_by_agency):
             slug = agency_slug(agency)
             body = foia_text + '\r\n\r\n' + slug
             slug_subject = subject + agency
@@ -108,7 +127,7 @@ def prep_agency_drafts(contacts_by_agency=[]):
                 body, slug_subject, contacts)}
             # TODO label the draft here not when you send so you can verify thanks
             drafts.append(draft)
-            print(draft)
+            print("Created draft", i, agency)
         # TODO verify all agencies have a draft ... some get skipped i.e. service errors
         return drafts
     else:
@@ -119,16 +138,17 @@ def sanity_check(drafts):
     """
     look before you leap
     """
-    print(drafts)
-    print('len(drafts)', len(drafts))
-    verify = input('Everything ready? [y/N]')
-    return verify in ['Y', 'y']
+    verify = input(
+        f'Found {len(drafts)} to send. Everything ready (y/[n])? ') or 'N'
+    return verify.lower() == 'y'
 
 
 def load_foia_text():
     """
     reads foia template from docx file as config'd
     """
+    from docx import Document
+
     return '\r\n'.join([p.text for p in Document(docx=foia_doc).paragraphs])
 
 
@@ -145,8 +165,8 @@ def compose_draft(body, subject, contacts):
             userId='me', body={'message': message}).execute()['id']
         draft = service.users().drafts().get(userId='me', id=draft_id).execute()
         return draft
-    except Exception as e:
-        print(e)
+    except Exception:
+        raise
 
 
 def compose_message(body, subject, contacts):
@@ -155,12 +175,18 @@ def compose_message(body, subject, contacts):
     using message text, subject, contacts
     and returns it encoded
     """
+    from email.mime.text import MIMEText
+    import base64
+
     message = MIMEText(body)
     message['subject'] = subject
     message['from'] = me
     message['to'] = contacts
+
+    raw = base64.urlsafe_b64encode(message.as_bytes())
+
     # return message
-    return {'raw': base64.urlsafe_b64encode(message.as_string())}
+    return {'raw': raw.decode()}
 
 
 def sender(draft):
@@ -168,8 +194,12 @@ def sender(draft):
     sends drafts with specified sleep interval,
     logs exceptions
     """
+    from time import sleep
+    from msg.label import label_agency
+
     agency = draft['agency']
     draft = draft['draft']
+
     try:
         sent = service.users().drafts().send(
             userId='me', body={'id': draft['id']}).execute()
@@ -182,6 +212,7 @@ def sender(draft):
         print('draft.id', draft['id'], 'raised exception: ', e)
         log.log_data(
             'msg', [{'draft_id': draft['id'], 'agency':agency, 'exception':e}])
+
     sleep(interval)
 
 
@@ -193,13 +224,14 @@ def delete_drafts(draft_ids=[]):
         # check for existence of drafts
         drafts = get_drafts()
         draft_ids = [x['id'] for x in drafts if type(drafts) == list]  # hack
-    print('len(draft_ids)', len(draft_ids))
-    dd = input('existing drafts found ... delete ?[y/N]')
-    if dd.lower() == 'y':
-        print(drafts)
-        for draft_id in draft_ids:
-            print('deleting', draft_id)
-            service.users().drafts().delete(userId='me', id=draft_id).execute()
+
+        if len(draft_ids) > 0:
+            dd = input(
+                f'{len(draft_ids)} existing drafts found ... delete (y,[n])? ') or 'n'
+            if dd.lower() == 'y':
+                for i, draft_id in enumerate(draft_ids):
+                    print('deleting', i, draft_id)
+                    service.users().drafts().delete(userId='me', id=draft_id).execute()
 
 
 def get_drafts():
@@ -208,6 +240,8 @@ def get_drafts():
     only called by delete_drafts, may be unnecessary
     """
     drafts = service.users().drafts().list(userId='me', maxResults=2000).execute()
+
     if 'drafts' in drafts.keys():
         drafts = drafts['drafts']
+
     return drafts
